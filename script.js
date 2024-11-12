37cef0d64916308d1e3513c845eda54e75c63aa6
Aslam <93756533+LW1117@users.noreply.github.com>
2024-11-12 11:47:00 +0530

    Change constructor to factory function

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+  return { title, author, pages, read };
 }
 
 function addBookToLibrary(bookToAdd) {
